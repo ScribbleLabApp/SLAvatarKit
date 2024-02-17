cd33443c0b0e33645a2a401a7098c5ed8365900e
N3v1 <n3v1010@gmail.com>
2024-02-17 17:45:16 +0100

    Initial Commit

---
+++ SLAvatarKit/SLAvatarKit.h
@@ -0,0 +1,18 @@
+//
+//  SLAvatarKit.h
+//  SLAvatarKit
+//
+//  Created by Nevio Hirani on 17.02.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SLAvatarKit.
+FOUNDATION_EXPORT double SLAvatarKitVersionNumber;
+
+//! Project version string for SLAvatarKit.
+FOUNDATION_EXPORT const unsigned char SLAvatarKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SLAvatarKit/PublicHeader.h>
+
+
