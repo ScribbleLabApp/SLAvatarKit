659518f3dbb4876fae3cb7fd60039d6be2102ba5
N3v1 <n3v1010@gmail.com>
2024-02-18 20:32:58 +0100

    [Chore]: Add swift example

---
+++ Example/swift-sample/avatar-explorer_swift/avatar-explorer_swift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  avatar-explorer_swift
+//
+//  Created by Nevio Hirani on 18.02.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Example/swift-sample/avatar-explorer_swift/avatar-explorer_swift/avatar_explorer_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  avatar_explorer_swiftApp.swift
+//  avatar-explorer_swift
+//
+//  Created by Nevio Hirani on 18.02.24.
+//
+
+import SwiftUI
+
+@main
+struct avatar_explorer_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
